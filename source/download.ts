bbb43b69d694ce88d5340e8064af9ad034477e79
EdJoPaTo <edjopato@gmail.com>
2020-03-19 02:31:36 +0100

    feat: allow downloading video without captions

--- source/download.ts
+++ source/download.ts
@@ -3,10 +3,13 @@ import childProcess from 'child_process'
 
 const exec = util.promisify(childProcess.exec)
 
-function downloadCommandLine(video: string, captions: string, targetfile: string): string {
+function downloadCommandLine(video: string, captions: string | undefined, targetfile: string): string {
 	let command = 'nice ffmpeg -y -v error'
 	command += ` -i "${video}"`
-	command += ` -i "${captions}"`
+	if (captions) {
+		command += ` -i "${captions}"`
+	}
+
 	command += ' -c copy  -c:s mov_text'
 	command += ' -codec:v h264'
 	if (process.env.NODE_ENV !== 'production') {
@@ -18,7 +21,7 @@ function downloadCommandLine(video: string, captions: string, targetfile: string
 	return command
 }
 
-export async function download(video: string, captions: string, targetfile: string): Promise<{stdout: string; stderr: string}> {
+export async function download(video: string, captions: string | undefined, targetfile: string): Promise<{stdout: string; stderr: string}> {
 	const command = downloadCommandLine(video, captions, targetfile)
 	const result = await exec(command)
 	return result
