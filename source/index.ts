42a58d2638ae62f650e487f074449cda9925bdc1
EdJoPaTo <edjopato@gmail.com>
2020-03-19 02:34:34 +0100

    feat: download zusatzsendungen

--- source/index.ts
+++ source/index.ts
@@ -8,7 +8,6 @@ import {sync} from './resilio'
 
 const TARGET_CHAT = '-1001214301516'
 const ERROR_TARGET = '-1001214301516'
-const BASE_URL = 'https://www.wdrmaus.de/aktuelle-sendung/'
 const FILE_PATH = './tmp/'
 
 if (process.env.NODE_ENV === 'production') {
@@ -19,26 +18,47 @@ const tokenFilePath = existsSync('/run/secrets') ? '/run/secrets/bot-token.txt' 
 const token = readFileSync(tokenFilePath, 'utf8').trim()
 const bot = new Telegraf(token)
 
-async function sendWhenNew(): Promise<void> {
+async function checkAktuelleSendung(): Promise<void> {
+	const BASE_URL = 'https://www.wdrmaus.de/aktuelle-sendung/'
 	const source = await request(BASE_URL)
 	const imgPart = /aktuelle-sendung\/([^"]+.jpg)/.exec(source)![1]
 	const img = BASE_URL + imgPart
 
+	const mediaObjJson = await getMediaObjJson(source)
+
+	return sendWhenNew('aktuelle-sendung', img, mediaObjJson)
+}
+
+async function checkZusatzsendung(): Promise<void> {
+	const BASE_URL = 'https://www.wdrmaus.de/'
+	const source = await request(BASE_URL + 'zusatzsendungen.php5')
+	const imgPart = /<img src="(imggen\/.+\.jpg)/.exec(source)![1]
+	const img = BASE_URL + imgPart
+
+	const mediaObjJson = await getMediaObjJson(source)
+
+	return sendWhenNew('zusatzsendung', img, mediaObjJson)
+}
+
+async function getMediaObjJson(source: string): Promise<any> {
 	const mediaObjUrl = /mediaObj': { 'url': '(https:[^']+)/.exec(source)![1]
 	const mediaObjString = await request(mediaObjUrl)
 	const mediaObjJsonString = mediaObjString
 		.replace('$mediaObject.jsonpHelper.storeAndPlay(', '')
 		.slice(0, -2)
 	const mediaObjJson = JSON.parse(mediaObjJsonString)
+	return mediaObjJson
+}
 
+async function sendWhenNew(context: string, img: string, mediaObjJson: any): Promise<void> {
 	const date: string = mediaObjJson.trackerData.trackerClipAirTime
 	const dateFilenamePart = parseDateToFilenamePart(date)
-	const filenamePrefix = FILE_PATH + 'wdrmaus-' + dateFilenamePart + '-'
+	const filenamePrefix = FILE_PATH + 'wdrmaus-' + context + '-' + dateFilenamePart + '-'
 
-	const last = await getLastRunMediaObj()
+	const last = await getLastRunMediaObj(context)
 	const areEqual = JSON.stringify(last) === JSON.stringify(mediaObjJson)
 	if (areEqual) {
-		console.log('same as last time')
+		console.log(context + ' same as last time')
 		// The last one is the same
 		return
 	}
@@ -47,13 +67,14 @@ async function sendWhenNew(): Promise<void> {
 		caption: date
 	}).notifications(false) as any)
 
-	await saveMediaObj(mediaObjJson)
+	await saveMediaObj(context, mediaObjJson)
 
 	const dgsVideo: string = mediaObjJson.mediaResource.dflt.slVideoURL
 	const normalVideo: string = mediaObjJson.mediaResource.dflt.videoURL
 	const captionsUrl: string = mediaObjJson.mediaResource.captionsHash.srt
 
 	let caption = ''
+	caption += '\n' + context
 	caption += '\n' + date
 	caption += '\nVideo: https:' + normalVideo
 	caption += '\nDGS: https:' + dgsVideo
@@ -61,7 +82,7 @@ async function sendWhenNew(): Promise<void> {
 
 	await bot.telegram.sendMessage(TARGET_CHAT, caption, Extra.inReplyTo(photoMessage.message_id) as any)
 
-	console.log('start download…')
+	console.log(`start download ${context}…`)
 	console.time('download')
 
 	console.time('download 1image')
@@ -70,7 +91,7 @@ async function sendWhenNew(): Promise<void> {
 	console.timeEnd('download 1image')
 
 	console.time('download 2normal')
-	await download('https:' + normalVideo, 'https:' + captionsUrl, filenamePrefix + '2normal.mp4')
+	await download('https:' + normalVideo, captionsUrl ? 'https:' + captionsUrl : undefined, filenamePrefix + '2normal.mp4')
 	console.timeEnd('download 2normal')
 
 	// Disable temporarily
@@ -82,14 +103,14 @@ async function sendWhenNew(): Promise<void> {
 	await bot.telegram.sendMessage(TARGET_CHAT, 'finished download', Extra.inReplyTo(photoMessage.message_id) as any)
 }
 
-async function getLastRunMediaObj(): Promise<any> {
-	const content = await fsPromises.readFile(FILE_PATH + 'last.json', 'utf8')
+async function getLastRunMediaObj(filename: string): Promise<any> {
+	const content = await fsPromises.readFile(FILE_PATH + filename + '.json', 'utf8')
 		.catch(() => '{}')
 	return JSON.parse(content)
 }
 
-async function saveMediaObj(mediaObj: any): Promise<void> {
-	return fsPromises.writeFile(FILE_PATH + 'last.json', JSON.stringify(mediaObj, null, 2), 'utf8')
+async function saveMediaObj(filename: string, mediaObj: any): Promise<void> {
+	return fsPromises.writeFile(FILE_PATH + filename + '.json', JSON.stringify(mediaObj, null, 2), 'utf8')
 }
 
 function parseDateToFilenamePart(date: string): string {
@@ -99,7 +120,8 @@ function parseDateToFilenamePart(date: string): string {
 
 async function run(): Promise<void> {
 	try {
-		await sendWhenNew()
+		await checkAktuelleSendung()
+		await checkZusatzsendung()
 	} catch (error) {
 		console.log(error)
 		bot.telegram.sendMessage(ERROR_TARGET, '```\n' + JSON.stringify(error, null, 2) + '\n```', Extra.markdown() as any)
