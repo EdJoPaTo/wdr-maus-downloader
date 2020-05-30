2f4cac19961bb99450bc82b7998880ed17a8294f
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-30 22:28:15 +0200

    feat: get current series MausBlickClari
    remove old ones

--- source/media-objects/currently-available.ts
+++ source/media-objects/currently-available.ts
@@ -4,7 +4,7 @@ import arrayFilterUnique from 'array-filter-unique'
 import {matchAll, sequentialAsync, ErrorHandler} from '../generics'
 import {parseMediaObjectJson} from './parse-media-obj'
 
-export type Context = 'AktuelleSendung' | 'Zusatzsendung' | 'Corona'
+export type Context = 'AktuelleSendung' | 'MausBlickClari'
 export interface Entry {
 	context: Context;
 	mediaObject: any;
@@ -14,13 +14,12 @@ export interface Entry {
 export async function getAll(errorHandler: ErrorHandler): Promise<Entry[]> {
 	return [
 		...await getAktuelleSendung(errorHandler),
-		...await getCorona(errorHandler),
-		...await getZusatzsendung(errorHandler)
+		...await getMausBlickClari(errorHandler)
 	]
 }
 
 async function getMediaObjectsFromSource(source: string): Promise<any[]> {
-	const allMediaUrls = matchAll(/mediaObj': { 'url': '(https:[^']+)/g, source)
+	const allMediaUrls = matchAll(/(https:[^'"]+\d+\.js)/g, source)
 		.map(o => o[1])
 
 	const allResponses = await sequentialAsync(async url => got(url), allMediaUrls)
@@ -30,7 +29,7 @@ async function getMediaObjectsFromSource(source: string): Promise<any[]> {
 }
 
 async function getAktuelleSendung(errorHandler: ErrorHandler): Promise<Entry[]> {
-	const context = 'AktuelleSendung'
+	const context: Context = 'AktuelleSendung'
 	try {
 		const BASE_URL = 'https://www.wdrmaus.de/aktuelle-sendung/'
 		const {body} = await got(BASE_URL)
@@ -58,41 +57,13 @@ async function getAktuelleSendung(errorHandler: ErrorHandler): Promise<Entry[]> 
 	}
 }
 
-async function getZusatzsendung(errorHandler: ErrorHandler): Promise<Entry[]> {
-	const context = 'Zusatzsendung'
+async function getMausBlickClari(errorHandler: ErrorHandler): Promise<Entry[]> {
+	const context: Context = 'MausBlickClari'
 	try {
-		const BASE_URL = 'https://www.wdrmaus.de/'
-		const {body} = await got(BASE_URL + 'zusatzsendungen.php5')
-
-		const imageUrls = matchAll(/<img src="(imggen\/.+\.jpg)/g, body)
-			.map(o => o[1])
-			.map(o => BASE_URL + o)
-
-		const mediaObjects = await getMediaObjectsFromSource(body)
-		if (mediaObjects.length !== imageUrls.length) {
-			throw new Error(`should find the same amount of images and video media objects ${imageUrls.length} != ${mediaObjects.length}`)
-		}
-
-		return imageUrls.map((o, i) => {
-			return {
-				context,
-				imageUrl: o,
-				mediaObject: mediaObjects[i]
-			}
-		})
-	} catch (error) {
-		await errorHandler(context, error)
-		return []
-	}
-}
-
-async function getCorona(errorHandler: ErrorHandler): Promise<Entry[]> {
-	const context = 'Corona'
-	try {
-		const BASE_URL = 'https://www.wdrmaus.de/extras/mausthemen/corona/'
+		const BASE_URL = 'https://www.wdrmaus.de/extras/mausthemen/mausblick/'
 		const {body} = await got(BASE_URL)
 
-		const imageUrls = matchAll(/<img src=".+(imggen\/.+\.jpg)/g, body)
+		const imageUrls = matchAll(/<img src="(imggen\/.+\.jpg)/g, body)
 			.map(o => o[1])
 			.map(o => BASE_URL + o)
 
