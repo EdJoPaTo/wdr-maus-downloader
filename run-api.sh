ff93c1b626d81eb69c706517978c0460d37d3cb3
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-03-01 11:19:04 +0100

    chore: add script to let the api run locally

---
+++ run-api.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+export TELEGRAM_API_HASH=
+export TELEGRAM_API_ID=
+export TELEGRAM_LOCAL=true
+
+podman run -p 8081:8081 --env TELEGRAM_API_ID --env TELEGRAM_API_HASH --env TELEGRAM_LOCAL docker.io/tdlight/tdlightbotapi
