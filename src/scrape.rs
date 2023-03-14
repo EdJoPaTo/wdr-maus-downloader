3c5d17e9931bbc5600bcd532c145fb427d31c7a6
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-03-14 17:07:10 +0100

    refactor: use core:: for impl
    not really relevant here but nice to default towards when
    thinking about no_std contexts

--- src/scrape.rs
+++ src/scrape.rs
@@ -12,8 +12,8 @@ pub enum Topic {
     Zukunft,
 }
 
-impl std::fmt::Display for Topic {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl core::fmt::Display for Topic {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{self:?}")
     }
 }
